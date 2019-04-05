90d956d38880080be98a484f56ad9c96ba6f3a30
Alexei Koshkin <alexeikoshkin@gmail.com>
2019-04-05 20:02:27 +0300

    Homework 9

--- Project Django/Alexeisite/Alexeisite/settings.py
+++ Project Django/Alexeisite/Alexeisite/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DEFAULT_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+MY_APPS = [
+    'contacts',
+    'main',
+    'reference'
+]
+
+INSTALLED_APPS = DEFAULT_APPS + MY_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
