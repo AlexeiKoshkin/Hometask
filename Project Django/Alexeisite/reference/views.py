1e333ee9745f1ea1177557b752c118b836a3fadc
Alexei Koshkin <alexeikoshkin@gmail.com>
2019-04-19 22:54:28 +0300

    Homework 13

--- Project Django/Alexeisite/reference/views.py
+++ Project Django/Alexeisite/reference/views.py
@@ -1,4 +1,6 @@
 from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+from django.views.generic.base import TemplateView
 from reference.models import Author, Serie, Genre, Publisher
 # Create your views here.
 
@@ -17,3 +19,23 @@ class GenreDetail(DetailView):
 
 class PublisherDetail(DetailView):
     model = Publisher
+
+
+class AuthorList(ListView):
+    model = Author
+
+
+class SerieList(ListView):
+    model = Serie
+
+
+class GenreList(ListView):
+    model = Genre
+
+
+class PublisherList(ListView):
+    model = Publisher
+
+
+class ReferenceVeiw(TemplateView):
+    template_name = 'reference/reference.html'
