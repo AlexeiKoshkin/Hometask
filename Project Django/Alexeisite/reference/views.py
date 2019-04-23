f34d50d36baa6c14d6be4fda26c7f24e54cc36dd
Alexei Koshkin <alexeikoshkin@gmail.com>
2019-04-23 23:47:15 +0300

    Homework 14

--- Project Django/Alexeisite/reference/views.py
+++ Project Django/Alexeisite/reference/views.py
@@ -2,6 +2,7 @@ from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from django.views.generic.base import TemplateView
 from reference.models import Author, Serie, Genre, Publisher
+from reference.forms import SearchForm
 # Create your views here.
 
 
@@ -24,17 +25,73 @@ class PublisherDetail(DetailView):
 class AuthorList(ListView):
     model = Author
 
+    def get_queryset(self):
+        qs = super().get_queryset()
+        search = self.request.GET.get('search', 0)
+        if search != 0:
+            return qs.filter(first_name=search)
+        else:
+            return qs
+
+    def get_context_data(self):
+        context = super().get_context_data()
+        f = SearchForm()
+        context["form"] = f
+        return context
+
 
 class SerieList(ListView):
     model = Serie
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        search = self.request.GET.get('search', 0)
+        if search != 0:
+            return qs.filter(name=search)
+        else:
+            return qs
+
+    def get_context_data(self):
+        context = super().get_context_data()
+        f = SearchForm()
+        context["form"] = f
+        return context
 
 
 class GenreList(ListView):
     model = Genre
 
+    def get_queryset(self):
+        qs = super().get_queryset()
+        search = self.request.GET.get('search', 0)
+        if search != 0:
+            return qs.filter(name=search)
+        else:
+            return qs
+
+    def get_context_data(self):
+        context = super().get_context_data()
+        f = SearchForm()
+        context["form"] = f
+        return context
+
 
 class PublisherList(ListView):
     model = Publisher
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        search = self.request.GET.get('search', 0)
+        if search != 0:
+            return qs.filter(name=search)
+        else:
+            return qs
+
+    def get_context_data(self):
+        context = super().get_context_data()
+        f = SearchForm()
+        context["form"] = f
+        return context
 
 
 class ReferenceVeiw(TemplateView):
